71ef037bdfdff80b888596994d0b346e67f6bf4e
Martin Charles <martincharles07@gmail.com>
2019-07-20 22:30:10 -0700

    Removed traversal Code
    This was a desprate attempt to use the kRPC stuff to collect
    data to make pretty graphs for the class. It isn't a great
    implementation but it works. It is a terrible citizen of the DHT
    network because it doesn't respond to any requests.

--- src/stream.rs
+++ src/stream.rs
@@ -1,16 +1,6 @@
 use futures::{
-    stream::{
-        FuturesUnordered,
-        StreamFuture,
-    },
-    Async,
     Future,
-    Poll,
     Stream,
-};
-use std::fmt::{
-    self,
-    Debug,
 };
 
 pub fn run_forever<S: Stream<Item = (), Error = ()>>(
@@ -21,60 +11,4 @@ pub fn run_forever<S: Stream<Item = (), Error = ()>>(
         .into_future()
         .map(|_| ())
         .map_err(|_| ())
-}
-
-pub fn select_all<I>(streams: I) -> SelectAll<I::Item>
-where
-    I: IntoIterator,
-    I::Item: Stream,
-{
-    let mut set = SelectAll::new();
-    for stream in streams {
-        set.push(stream);
-    }
-    return set;
-}
-
-pub struct SelectAll<S> {
-    inner: FuturesUnordered<StreamFuture<S>>,
-}
-
-impl<T: Debug> Debug for SelectAll<T> {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(fmt, "SelectAll {{ ... }}")
-    }
-}
-impl<S: Stream> SelectAll<S> {
-    fn new() -> SelectAll<S> {
-        SelectAll {
-            inner: FuturesUnordered::new(),
-        }
-    }
-
-    pub fn push(&mut self, stream: S) {
-        self.inner.push(stream.into_future());
-    }
-}
-
-impl<S: Stream> Stream for SelectAll<S> {
-    type Item = S::Item;
-    type Error = S::Error;
-
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        loop {
-            match self.inner.poll() {
-                Ok(Async::NotReady) => return Ok(Async::NotReady),
-                Ok(Async::Ready(Some((Some(item), remaining)))) => {
-                    self.push(remaining);
-                    return Ok(Async::Ready(Some(item)));
-                }
-                Err((err, remaining)) => {
-                    self.push(remaining);
-                    return Err(err);
-                }
-                Ok(Async::Ready(Some((None, _remaining)))) => {}
-                Ok(Async::Ready(None)) => return Ok(Async::Ready(None)),
-            }
-        }
-    }
 }
